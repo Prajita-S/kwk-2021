a5d68b1bb53b7c4dc36872161af6a6829a9e5c34
Prajita-S <87436933+Prajita-S@users.noreply.github.com>
2021-07-14 14:05:49 -0400

    Add files via upload

--- kwk 2021/Intro-to-Xcode&Storyboard/Intro-to-Xcode&Storyboard/ViewController.swift
+++ kwk 2021/Intro-to-Xcode&Storyboard/Intro-to-Xcode&Storyboard/ViewController.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var appTitle: UILabel!
+    
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("This is a print statement - testing testing!")
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: UIButton)/* function's arguement*/ {
+        
+        if let newTitle = textField.text{
+            appTitle.text = newTitle
+        }
+        
+    }
+    
 }
 
