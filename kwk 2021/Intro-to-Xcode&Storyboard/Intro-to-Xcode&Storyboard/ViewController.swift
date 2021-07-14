897b300aa389316ca259171fb9d320060add836d
Prajita-S <87436933+Prajita-S@users.noreply.github.com>
2021-07-14 11:37:28 -0400

    1st version

---
+++ kwk 2021/Intro-to-Xcode&Storyboard/Intro-to-Xcode&Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intro-to-Xcode&Storyboard
+//
+//  Created by  Scholar on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("This is a print statement - testing testing!")
+    }
+
+
+}
+
