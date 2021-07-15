efdfecf7be016627ba3c6866d357164f77990731
Prajita-S <87436933+Prajita-S@users.noreply.github.com>
2021-07-15 01:15:13 -0400

    Add files via upload

---
+++ kwk 2021/Intro-to-Xcode&Storyboard/kwk 2021/About Me/About Me/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by  Scholar on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var infoText: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func factButton(_ sender: UIButton) {
+    }
+    
+}
+
