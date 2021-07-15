eae011bac6e76c1e4248554a508351bf71975971
Prajita-S <87436933+Prajita-S@users.noreply.github.com>
2021-07-15 10:35:49 -0400

    Add files via upload

--- kwk 2021/About Me/About Me/ViewController.swift
+++ kwk 2021/About Me/About Me/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainImage: UIImageView!
     @IBOutlet var infoText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +17,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func factButton(_ sender: UIButton) {
+        let infoText.text = "I was born and raised in Nepal where I stayed 9 years of my life."
     }
     
 }
