3a36a7a567f95ad8f3f59f5074f3e33ea69fd53d
Prajita-S <87436933+Prajita-S@users.noreply.github.com>
2021-07-19 11:36:12 -0400

    Add files via upload

---
+++ ToDo's 2/ToDo's 2/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDo's 2
+//
+//  Created by  Scholar on 7/19/21.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = "" //empty string
+    var important = false //false boolean
+}
